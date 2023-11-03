7f62da6fbba7b5a7d6b9f07e8e6c9e03737e0ca7
Rosa <rosa.rezaei17@gmail.com>
2023-11-03 01:41:08 -0700

    production plugins added

---
+++ config/env/production/plugins.js
@@ -0,0 +1,42 @@
+module.exports = ({ env }) => ({
+    graphql: {
+      config: {
+        endpoint: '/graphql',
+        shadowCRUD: true,
+        playgroundAlways: true,
+        depthLimit: 10,
+        amountLimit: 100,
+        apolloServer: {
+          tracing: false,
+          introspection: true,
+        },
+      },
+    },
+    upload: {
+        config: {
+          provider: 'cloudinary',
+          providerOptions: {
+            cloud_name: env('CLOUDINARY_NAME'),
+            api_key: env('CLOUDINARY_KEY'),
+            api_secret: env('CLOUDINARY_SECRET'),
+          },
+          actionOptions: {
+            upload: {},
+            uploadStream: {},
+            delete: {},
+          },
+        },
+      },
+      email: {
+        config: {
+          provider: 'strapi-provider-email-brevo',
+          providerOptions: {
+            apiKey: env('BREVO_API_KEY'),
+          },
+          settings: {
+            defaultFrom: 'rosa.rezaei17@gmail.com',
+            defaultReplyTo: 'rosa.rezaei17@gmail.com',
+          },
+        },
+      },
+});
